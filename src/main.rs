b0ab90006dc4b205fea7e392a336c91d0e72dd52
Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>
2023-11-28 17:37:55 -0600

    WiP
    Signed-off-by: Jadin Heaston <86203688+JadinHeaston@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use config::Config;
 use md5::{Digest, Md5};
 use rayon::prelude::*;
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::sync::Mutex;
 use std::{fmt, path::PathBuf};
@@ -8,13 +9,19 @@ use std::{fmt, path::PathBuf};
 // const THREAD_COUNT: usize = 4;
 const DIRECTORY_KEYS: [&str; 2] = ["Directory One", "Directory Two"];
 
+struct File {
+    path: PathBuf,
+    hash: Option<[u8; 16]>,
+    metadata: std::fs::Metadata,
+}
+
 mod config_options;
 
 fn main() {
     use std::time::Instant;
     let now = Instant::now();
 
-    println!("Started - Loading configuration");
+    println!("{:.2?} Started - Loading configuration", now.elapsed());
     let config = Config::builder()
         .add_source(config::File::with_name("config"))
         .build()
@@ -35,72 +42,70 @@ fn main() {
                 .unwrap(),
         );
     }
-    println!("Finished - Loading configuration");
+    println!("{:.2?} Finished - Loading configuration", now.elapsed());
 
-    println!("Reading directories...");
+    println!("{:.2?} Started - Reading directories", now.elapsed());
 
-    let directory_files: Mutex<HashMap<&str, Vec<std::path::PathBuf>>> = Mutex::new(HashMap::new());
+    let directory_files: Mutex<HashMap<&str, Vec<File>>> = Mutex::new(HashMap::new());
 
     //Reading directories.
     DIRECTORY_KEYS.par_iter().for_each(|directory_key| {
-        println!("Reading directory: {}", directory_key);
         let directory_config = directory_configs
             .get_key_value(directory_key)
             .unwrap()
             .1
             .clone();
+        let directory = directory_config
+            .get_key_value("Path")
+            .unwrap()
+            .1
+            .clone()
+            .into_string()
+            .unwrap();
+        let recursive_flag = directory_config
+            .get_key_value("Recursive")
+            .unwrap()
+            .1
+            .clone()
+            .into_bool()
+            .unwrap();
 
-        let files = get_files_in_directory(
-            directory_config
-                .get_key_value("Path")
-                .unwrap()
-                .1
-                .clone()
-                .into_string()
-                .unwrap(),
-            directory_config
-                .get_key_value("Recursive")
-                .unwrap()
-                .1
-                .clone()
-                .into_bool()
-                .unwrap(),
-        )
-        .unwrap();
+        let files = get_files_in_directory(directory.clone(), recursive_flag).unwrap();
 
         directory_files.lock().unwrap().insert(directory_key, files);
     });
+    println!("{:.2?} Finished - Reading directories", now.elapsed());
 
-    println!(
-        "Files in directory: {:#?}",
-        directory_files.lock().unwrap().clone()
-    );
+    // println!(
+    //     "Files in directory: {:#?}",
+    //     directory_files.lock().unwrap().clone()
+    // );
 
     // println!("Starting - Performing initial comparison");
     // println!("Finished - Performing initial comparison");
 
-    println!("Starting - Hashing");
+    println!("{:.2?} Starting - Hashing", now.elapsed());
     directory_files
         .lock()
         .unwrap()
         .par_iter()
         .for_each(|(_, files)| {
             for file in files {
-                let hash = hash_file(&file);
-                println!("{:#?}", hash);
+                let hash = Some(hash_file(&file.path));
+                // println!("{:#?}", hex::encode(hash));
             }
         });
-    println!("Finished - Hashing");
+    println!("{:.2?} Finished - Hashing", now.elapsed());
 
     let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+    println!("Ttoal elapsed: {:.2?}", elapsed);
 }
 
 fn get_files_in_directory(
     directory: String,
     subdirectories: bool,
-) -> Result<Vec<std::path::PathBuf>, std::io::Error> {
-    let mut file_list: Vec<std::path::PathBuf> = Vec::new();
+) -> Result<Vec<File>, std::io::Error> {
+    let mut file_list: Vec<File> = Vec::new();
     if subdirectories.clone() == true {
         for entry in walkdir::WalkDir::new(directory)
             .follow_links(true)
@@ -108,23 +113,38 @@ fn get_files_in_directory(
             .filter_map(|e| e.ok())
         {
             if entry.path().is_file() {
-                file_list.push(entry.path().to_path_buf());
+                file_list.push(File {
+                    path: entry.path().to_path_buf(),
+                    hash: None,
+                    metadata: std::fs::metadata(entry.path()).unwrap(),
+                });
             }
         }
     } else {
         for entry in std::path::Path::new(&directory).read_dir().unwrap() {
             let entry = entry.unwrap();
             if entry.path().is_file() {
-                file_list.push(entry.path());
+                file_list.push(File {
+                    path: entry.path().to_path_buf(),
+                    hash: None,
+                    metadata: std::fs::metadata(entry.path()).unwrap(),
+                });
             }
         }
     }
     return Ok(file_list);
 }
 
-fn hash_file(file_path: &PathBuf) -> String {
+fn debug_hash_file(file_path: &PathBuf) -> String {
     let mut hasher = Md5::new();
     hasher.update(std::fs::read(file_path).unwrap());
     let hash = hasher.clone().finalize();
     return fmt::format(format_args!("{:x}", hash));
+}
+
+fn hash_file(file_path: &PathBuf) -> [u8; 16] {
+    let mut hasher = Md5::new();
+    hasher.update(std::fs::read(file_path).unwrap());
+    let hash = hasher.clone().finalize();
+    return hash.into();
 }
